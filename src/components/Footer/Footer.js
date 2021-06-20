e62a913bd0395734af1664d7f1e2e5b3544f5630
srikanth <srikanthkongara91@gmail.com>
2021-06-20 12:45:43 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./Footer.style";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <S.Footer>
+        <h3>Footer</h3>
+      </S.Footer>
+    );
+  }
+}
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./Header.style";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <S.Header>
+        <h1>Prime Image Choice</h1>
+      </S.Header>
+    );
+  }
+}
+export default Header;
