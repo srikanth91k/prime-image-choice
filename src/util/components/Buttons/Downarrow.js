8b57d25a2d5503a7205304e6a56a0a3e2241c133
srikanth <srikanthkongara91@gmail.com>
2021-06-24 22:41:58 +0530

    fourth commit

---
+++ src/util/components/Buttons/Downarrow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ICON } from "../../icons/Icon";
+import * as S from "./Button.style";
+
+const Downarrow = () => {
+  return (
+    <S.Downarrow>
+      <ICON.ARROW_DROPDOWN />
+    </S.Downarrow>
+  );
+};
+
+export default Downarrow;
